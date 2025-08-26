5ef720e644b40621e3961c8c2ed2a07241119af5
sesamehowie <spooolieyeah@gmail.com>
2025-08-26 14:08:58 +0300

    additional checks

--- data/config.py
+++ data/config.py
@@ -1,3 +1,5 @@
 SLEEP_INTERVAL = [2, 5]
 SLEEP_ON_RATELIMIT = [10, 15]
 DEPOSIT_ADDRESS = ""
+KEYS_PATH = "input_data/private_keys.txt"
+PROXIES_PATH = "input_data/private_keys.txt"
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from concurrent.futures import ThreadPoolExecutor
 
@@ -6,20 +7,29 @@ from loguru import logger
 from functions import account_wrapper
 from models.logger import configure_logger
 from utils.file_utils import read_txt
+from data.config import KEYS_PATH, PROXIES_PATH
 
 
 def main():
     configure_logger("logs/")
-    private_keys = read_txt("input_data/private_keys.txt")
-    proxies = read_txt("input_data/proxies.txt")
 
+    if not os.path.exists(KEYS_PATH) or not os.path.exists(PROXIES_PATH):
+        logger.info("Private key and proxy files should exist in the configured path.")
+        sys.exit(1)
+
+    private_keys = read_txt(KEYS_PATH)
+    proxies = read_txt(PROXIES_PATH)
     private_keys_amt = len(private_keys)
     proxies_amt = len(proxies)
 
+    if private_keys_amt == 0 or proxies_amt == 0:
+        logger.info("Private keys and/or proxies amount should not be 0.")
+        sys.exit(1)
+
     logger.info(f"Loaded {private_keys_amt} private keys and {proxies_amt} proxies.")
 
-    if private_keys_amt != proxies_amt or private_keys_amt == 0 or proxies_amt == 0:
-        logger.info("Amount of private keys and proxies should match. Exiting...")
+    if private_keys_amt != proxies_amt:
+        logger.info("Amount of private keys and proxies should match.")
         sys.exit(1)
 
     account_names = [str(i) for i in range(private_keys_amt)]
