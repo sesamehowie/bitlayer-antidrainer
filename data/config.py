5c693bbef0bf70dc65b15e99010f4c933c5158fd
sesamehowie <spooolieyeah@gmail.com>
2025-08-26 14:10:02 +0300

    typo fix

--- data/config.py
+++ data/config.py
@@ -2,4 +2,4 @@ SLEEP_INTERVAL = [2, 5]
 SLEEP_ON_RATELIMIT = [10, 15]
 DEPOSIT_ADDRESS = ""
 KEYS_PATH = "input_data/private_keys.txt"
-PROXIES_PATH = "input_data/private_keys.txt"
+PROXIES_PATH = "input_data/proxies.txt"
